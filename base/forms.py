aae924f85d4717191632aaf4df753570d79f2136
Elyes159 <elyesmlik307@gmail.com>
2024-04-15 09:28:01 +0100

    Your commit message here

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,65 @@
 from django.contrib.auth.forms import  AuthenticationForm
 from django.contrib.auth.models import User
 
+from base.models import ResponsableJuridique, ResponsableMaintenance
+from django import forms
+from .models import Admin
+
+
 class LoginForm(AuthenticationForm) :
     class Meta :
         model = User
+        fields = [ 
+            "username",
+            "password"
+        ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['username'].label = ''
+        # self.fields['password1'].label = ''
+        self.fields['username'].help_text = ''
+        self.fields['password'].help_text = ''
+        self.fields['username'].widget.attrs.update({'class':"form-control","id":"exampleInputEmail1","placeholder":"Username"})
+        self.fields['password'].widget.attrs.update({'class':"form-control","id":"exampleInputEmail1","placeholder":"Password", "data-toggle":"password"})
+        
+        
+class LoginFormJ(AuthenticationForm) :
+    class Meta :
+        model = ResponsableJuridique
+        fields = [ 
+            "username",
+            "password"
+        ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['username'].label = ''
+        # self.fields['password1'].label = ''
+        self.fields['username'].help_text = ''
+        self.fields['password'].help_text = ''
+        self.fields['username'].widget.attrs.update({'class':"form-control","id":"exampleInputEmail1","placeholder":"Username"})
+        self.fields['password'].widget.attrs.update({'class':"form-control","id":"exampleInputEmail1","placeholder":"Password", "data-toggle":"password"})
+        
+class LoginFormM(AuthenticationForm) :
+    class Meta :
+        model = ResponsableMaintenance
+        fields = [ 
+            "username",
+            "password"
+        ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['username'].label = ''
+        # self.fields['password1'].label = ''
+        self.fields['username'].help_text = ''
+        self.fields['password'].help_text = ''
+        self.fields['username'].widget.attrs.update({'class':"form-control","id":"exampleInputEmail1","placeholder":"Username"})
+        self.fields['password'].widget.attrs.update({'class':"form-control","id":"exampleInputEmail1","placeholder":"Password", "data-toggle":"password"})
+        
+        
+
+class AdminForm(AuthenticationForm) :
+    class Meta :
+        model = Admin
         fields = [ 
             "username",
             "password"
--- base/urls.py
+++ base/urls.py
@@ -30,5 +30,19 @@ urlpatterns = [
     path('acces /', acces  , name='acces'),
     path('consultant /', consultant , name='consultant'),
     
-    
+    path('home/', views.home_view, name='home'),
+    path("signup/", authView, name="authView"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('admin_interface/', admin_interface, name='admin_interface'),
+    path('res_maintenace/', res_maintenace, name='res_maintenace'),
+    path('responsable-juridique/', responsable_juridique, name='responsable_juridique'),
+    path('add_site/', add_site_view, name='add_site'),  
+    path('add_jui/', add_jui_view, name='add_jui'),
+    path('modify_jui/', modify_jui, name='modify_jui'),
+    path('delete_jui/', delete_jui, name='delete_jui'),   
+    path('accounts/login/', views.login_user_view),
+    path('login/ResponsableJuridique/',views.login_juridique_view,name="login_responsable_juridique"),#hedha lien mta3 login de responsable juridique
+    path('login/ResponsableManitenance/',views.login_maintenance_view,name="login_maintenance_view"),
+    path('loginss/',views.loginss_view), # lien bech te5tar en tant que chkoun bech taamel login
+    path('login/Adiministrator',views.login_admin_view,name = "login_admin_view")   
 ]     
